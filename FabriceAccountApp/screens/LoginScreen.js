c681033d240f7d187cb55a786d37be67f93d0146
ihongui <fihongui@st.ug.edu.gh>
2024-06-17 01:48:20 +0900

    finally i'm done with login page

--- FabriceAccountApp/screens/LoginScreen.js
+++ FabriceAccountApp/screens/LoginScreen.js
@@ -19,7 +19,9 @@ const LoginScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.logo}>Jobizz</Text>
+      <View style={styles.header}>
+        <Text style={styles.logoText}>Solutech</Text>
+      </View>
       <Text style={styles.welcomeText}>Welcome Back 👋</Text>
       <Text style={styles.subtitle}>Let's log in. Apply to jobs!</Text>
 
@@ -40,17 +42,19 @@ const LoginScreen = ({ navigation }) => {
         <Text style={styles.loginButtonText}>Log in</Text>
       </TouchableOpacity>
 
+      <View style={styles.line}></View>
       <Text style={styles.orText}>Or continue with</Text>
+      <View style={styles.line}></View>
 
       <View style={styles.socialLoginContainer}>
         <TouchableOpacity style={styles.socialLoginButton}>
-          <Icon name="apple" type="font-awesome" size={30} />
+          <Icon name="apple" type="font-awesome" size={30} color="black" />
         </TouchableOpacity>
         <TouchableOpacity style={styles.socialLoginButton}>
-          <Icon name="google" type="font-awesome" size={30} />
+          <Icon name="google" type="font-awesome" size={30} color="#DB4437" />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.socialLoginButton}>
-          <Icon name="facebook" type="font-awesome" size={30} />
+        <TouchableOpacity style={styles.socialLoginButton1}>
+          <Icon name="facebook" type="font-awesome" size={30} color="white" />
         </TouchableOpacity>
       </View>
 
@@ -69,21 +73,39 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: "#f8f8f8",
   },
-  logo: {
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    position: "absolute",
+    top: 30,
+    left: 20,
+  },
+
+  logoText: {
     fontSize: 32,
     fontWeight: "bold",
     color: "#3b5998",
-    marginBottom: 20,
   },
   welcomeText: {
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    position: "absolute",
+    top: 90,
+    left: 20,
   },
   subtitle: {
     fontSize: 16,
     color: "#666",
     marginBottom: 20,
+    marginBottom: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    position: "absolute",
+    top: 130,
+    left: 20,
   },
   input: {
     height: 48,
@@ -94,6 +116,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     marginBottom: 12,
     backgroundColor: "#fff",
+    top: 12,
   },
   loginButton: {
     backgroundColor: "#3b5998",
@@ -103,6 +126,7 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center",
     marginBottom: 16,
+    top: 15,
   },
   loginButtonText: {
     color: "#fff",
@@ -113,6 +137,13 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: "#666",
     marginVertical: 16,
+    top: 0,
+  },
+  line: {
+    height: 1,
+    backgroundColor: "#ddd",
+    width: "100%",
+    marginVertical: 10,
   },
   socialLoginContainer: {
     flexDirection: "row",
@@ -132,14 +163,31 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 4,
     elevation: 2,
+    top: 30,
+  },
+  socialLoginButton1: {
+    width: 50,
+    height: 50,
+    backgroundColor: "blue",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 2,
+    top: 30,
   },
   registerText: {
     fontSize: 14,
     color: "#666",
+    top: 50,
   },
   registerLink: {
     color: "#3b5998",
     fontWeight: "bold",
+    top: 50,
   },
 });
 
